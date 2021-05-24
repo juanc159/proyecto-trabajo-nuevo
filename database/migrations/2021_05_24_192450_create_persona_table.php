843f132b8472f3ccf4b898bfc9f7e9be24a932db
David <David>
2021-05-24 14:29:41 -0500

    iniciando proyecto prueba

---
+++ database/migrations/2021_05_24_192450_create_persona_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persona', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombrePersona');
+            $table->foreignId('idSexo')->references('id')->on('sexo');
+            $table->foreignId('idTipoDocumento')->references('id')->on('tipo_documento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persona');
+    }
+}
